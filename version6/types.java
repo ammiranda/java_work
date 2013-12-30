c19c69a09d0c241e6effc365a7949217d0ef53ba
Alexander <alexandermichaelmiranda@gmail.com>
2013-12-30 10:03:37 -0800

    Added strongly typed exercise

---
+++ version6/types.java
@@ -0,0 +1,12 @@
+public class types {
+    public static void main(String[] args) {
+        byte zero = 0;
+        short a = 3;
+        int b = 1;
+        char d = ' ';
+        double e = 2.0;
+        boolean f = true;
+        String output = "H" + a + b + b + zero + d + "w" + zero + "r" + b + "d" + d + e + d + f;
+        System.out.println(output);
+    }
+}
